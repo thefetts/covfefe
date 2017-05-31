11e29bb4fbf4f07b79c0030d2739ae705607aa07
Jordan Fallon <thefetts@gmail.com>
2017-05-31 17:58:39 -0600

    Use TreeWalker API! Neat!

--- covfefe.js
+++ covfefe.js
@@ -1,27 +1,23 @@
 traverse(document.body);
 
-function traverse(node) {
-  // credit goes to Steven Frank of Cloud to Butt (https://github.com/panicsteve/cloud-to-butt/)
-  switch(node.nodeType) {
-    case 1:
-    case 9:
-    case 11:
-      let child = node.firstChild;
-      while(child) {
-        const next = child.nextSibling;
-        traverse(child);
-        child = next;
-      }
-      break;
-    case 3:
-      covfefe(node);
-      break;
-  }
+function traverse(root) {
+  let node, walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false);
+  while (node = walker.nextNode())
+    covfefe(node)
 }
 
 function covfefe(node) {
   const realWords = ['coverage', 'coffee', 'coven', 'cover', 'covet', 'covey', 'cuvee', 'coffer', 'couvade', 'cuvette',
     'coeval', 'colville', 'cloven', 'clover', 'cuvier', 'caveat', 'cavite', 'cavell', 'cuvée'];
-  realWords.forEach(realWord =>
-    node.nodeValue = node.nodeValue.replace(new RegExp(realWord, 'gi'), 'covfefe'));
+  realWords.forEach(realWord => node.nodeValue = Covfefe(node.nodeValue, realWord));
+}
+
+function Covfefe(text, realWord) {
+  return text
+    .replace(new RegExp(capFirst(realWord), 'g'), 'Covfefe')
+    .replace(new RegExp(realWord, 'g'), 'covfefe')
+}
+
+function capFirst(text) {
+  return text.slice(0, 1).toUpperCase() + text.slice(1);
 }
